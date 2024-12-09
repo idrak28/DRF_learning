906fd0247f4864fa99f9ea951d6dcce0f2f1d8f8
idrak28 <idrakefaz99@gmail.com>
2024-12-09 20:58:17 +0600

    first commit

---
+++ api/views.py
@@ -0,0 +1,24 @@
+from django.http import JsonResponse
+from api.serializers import ProductSerializer
+from api.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+
+
+
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many =True) #many add when more than one elements
+   # return JsonResponse({
+    # 'data' : serializer .data     ###normal django
+   # })
+    return Response(serializer.data) ### for DRF
+
+@api_view(['GET'])
+def product_detail(request,pk):
+    product = get_object_or_404(Product,pk=pk)   # single product
+    serializer = ProductSerializer(product)
+  
+    return Response(serializer.data)
